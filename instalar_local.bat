3bba9f6a6d8e86cd97bbb3549b85823f29c9fdcb
omarazabache24-debug <omar.azabache24@gmail.com>
2026-05-20 23:25:48 -0500

    Delete instalar_local.bat

--- instalar_local.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Instalando dependencias...
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-echo.
-echo Instalacion finalizada. Ejecuta: python app.py
-pause
